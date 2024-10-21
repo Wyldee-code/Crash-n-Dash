1e41cde0696a8b44f15b16f316b1eed0809a536e
Kayleen Sandoval <kayleen.sandoval02@gmail.com>
2024-10-21 17:09:33 -0500

    running

--- backend/db/models/index.js
+++ backend/db/models/index.js
@@ -1,3 +1,42 @@
+// 'use strict';
+
+// const fs = require('fs');
+// const path = require('path');
+// const Sequelize = require('sequelize');
+// const process = require('process');
+// const basename = path.basename(__filename);
+// const env = process.env.NODE_ENV || 'development';
+// const config = require(__dirname + '/../../config/database.js')[env];
+// const db = {};
+
+// let sequelize;
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+
+// fs
+//   .readdirSync(__dirname)
+//   .filter(file => {
+//     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+//   })
+//   .forEach(file => {
+//     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+//     db[model.name] = model;
+//   });
+
+// Object.keys(db).forEach(modelName => {
+//   if (db[modelName].associate) {
+//     db[modelName].associate(db);
+//   }
+// });
+
+// db.sequelize = sequelize;
+// db.Sequelize = Sequelize;
+
+// module.exports = db;
+
 'use strict';
 
 const fs = require('fs');
@@ -11,28 +50,43 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
+  // Ensure the environment variable is correctly read
+  if (!process.env[config.use_env_variable]) {
+    console.error(`Environment variable ${config.use_env_variable} not set.`);
+    process.exit(1);
+  }
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+try {
+  fs
+    .readdirSync(__dirname)
+    .filter(file => {
+      return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    })
+    .forEach(file => {
+      const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+      if (!model.name) {
+        throw new Error(`Model in file ${file} does not export a valid name.`);
+      }
+      console.log(`Loading model - ${model.name}`);  // Logs the name of each model being loaded
+      db[model.name] = model;
+    });
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+  Object.keys(db).forEach(modelName => {
+    if (db[modelName].associate) {
+      db[modelName].associate(db);
+    }
+  });
+} catch (error) {
+  console.error(`Error setting up models: ${error.message}`);
+  process.exit(1); // Exit if there's a failure in model setup
+}
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
+
--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,20 +1,149 @@
+// 'use strict';
+// const { Model, Validator } = require('sequelize');
+// const bcrypt = require('bcryptjs');
+
+// module.exports = (sequelize, DataTypes) => {
+//   class User extends Model {
+//     toSafeObject() {
+//       const { id, username, email, firstName, lastName } = this; // context will be the User instance
+//       return { id, username, email, firstName, lastName };
+//     }
+//     validatePassword(password) {
+//       return bcrypt.compareSync(password, this.hashedPassword.toString());
+//     }
+
+//     static getCurrentUserById(id) {
+//       return User.scope("currentUser").findByPk(id);
+//     }
+//     static async login({ credential, password }) {
+//       const { Op } = require('sequelize');
+//       const user = await User.scope('loginUser').findOne({
+//         where: {
+//           [Op.or]: {
+//             username: credential,
+//             email: credential
+//           }
+//         }
+//       });
+//       if (user && user.validatePassword(password)) {
+//         return await User.scope('currentUser').findByPk(user.id);
+//       }
+//     }
+//     static async signup({ username, email, password, firstName, lastName }) {
+//       const hashedPassword = bcrypt.hashSync(password);
+//       const user = await User.create({
+//         username,
+//         email,
+//         hashedPassword,
+//         firstName,
+//         lastName
+//       });
+//       return await User.scope('currentUser').findByPk(user.id);
+//     }
+//     static associate(models) {
+//       User.hasMany(models.Spot, { foreignKey: "ownerId" });
+//       User.hasMany(models.Booking, { foreignKey: "userId" });
+//       User.hasMany(models.Review, { foreignKey: "userId" });
+//     }
+//   };
+
+//   User.init(
+//     {
+//       username: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//         validate: {
+//           len: [4, 30],
+//           isNotEmail(value) {
+//             if (Validator.isEmail(value)) {
+//               throw new Error("Cannot be an email.");
+//             }
+//           }
+//         }
+//       },
+//       firstName: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//         validate: {
+//           len: [1, 30],
+//         }
+//       },
+//       lastName: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//         validate: {
+//           len: [1, 30],
+//         }
+//       },
+//       email: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//         validate: {
+//           len: [3, 256],
+//           isEmail: true
+//         }
+//       },
+//       hashedPassword: {
+//         type: DataTypes.STRING.BINARY,
+//         allowNull: false,
+//         validate: {
+//           len: [60, 60]
+//         }
+//       }
+//     },
+//     {
+//       sequelize,
+//       modelName: "User",
+//       defaultScope: {
+//         attributes: {
+//           exclude: ["hashedPassword", "email", "createdAt", "updatedAt"]
+//         }
+//       },
+//       scopes: {
+//         currentUser: {
+//           attributes: { exclude: ["hashedPassword", "createdAt", "updatedAt"] }
+//         },
+//         loginUser: {
+//           attributes: {}
+//         }
+//       }
+//     }
+//   );
+//   return User;
+// };
+
+
 'use strict';
-const { Model, Validator } = require('sequelize');
+const { Model } = require('sequelize');
 const bcrypt = require('bcryptjs');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
+    /**
+     * Returns a safe, limited set of user fields for response objects.
+     */
     toSafeObject() {
-      const { id, username, email, firstName, lastName } = this; // context will be the User instance
+      const { id, username, email, firstName, lastName } = this;
       return { id, username, email, firstName, lastName };
     }
+
+    /**
+     * Validates a password against the hashed password in the database.
+     */
     validatePassword(password) {
       return bcrypt.compareSync(password, this.hashedPassword.toString());
     }
 
+    /**
+     * Retrieves a user by ID with restricted scope to exclude sensitive fields.
+     */
     static getCurrentUserById(id) {
       return User.scope("currentUser").findByPk(id);
     }
+
+    /**
+     * Authenticates a user with a given credential and password.
+     */
     static async login({ credential, password }) {
       const { Op } = require('sequelize');
       const user = await User.scope('loginUser').findOne({
@@ -26,11 +155,16 @@ module.exports = (sequelize, DataTypes) => {
         }
       });
       if (user && user.validatePassword(password)) {
-        return await User.scope('currentUser').findByPk(user.id);
+        return User.scope('currentUser').findByPk(user.id);
       }
+      throw new Error('Invalid username or password');
     }
+
+    /**
+     * Registers a new user with provided details and hashes their password.
+     */
     static async signup({ username, email, password, firstName, lastName }) {
-      const hashedPassword = bcrypt.hashSync(password);
+      const hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
       const user = await User.create({
         username,
         email,
@@ -38,76 +172,84 @@ module.exports = (sequelize, DataTypes) => {
         firstName,
         lastName
       });
-      return await User.scope('currentUser').findByPk(user.id);
+      return User.scope('currentUser').findByPk(user.id);
     }
-    static associate(models) {
-      User.hasMany(models.Spot, { foreignKey: "ownerId" });
-      User.hasMany(models.Booking, { foreignKey: "userId" });
-      User.hasMany(models.Review, { foreignKey: "userId" });
-    }
-  };
 
-  User.init(
-    {
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [4, 30],
-          isNotEmail(value) {
-            if (Validator.isEmail(value)) {
-              throw new Error("Cannot be an email.");
-            }
+    /**
+     * Defines model associations.
+     */
+    static associate(models) {
+      // Checks if associated models are loaded before setting up associations
+      if (!models.Spot || !models.Booking || !models.Review) {
+        throw new Error('Model associations failed due to missing models');
+      }
+      User.hasMany(models.Spot, { foreignKey: 'ownerId' });
+      User.hasMany(models.Booking, { foreignKey: 'userId' });
+      User.hasMany(models.Review, { foreignKey: 'userId' });
+    }
+  }
+
+  User.init({
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4, 30],
+        isNotEmail(value) {
+          if (Validator.isEmail(value)) {
+            throw new Error("Username cannot be an email.");
           }
-        }
-      },
-      firstName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1, 30],
-        }
-      },
-      lastName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1, 30],
-        }
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [3, 256],
-          isEmail: true
-        }
-      },
-      hashedPassword: {
-        type: DataTypes.STRING.BINARY,
-        allowNull: false,
-        validate: {
-          len: [60, 60]
         }
       }
     },
-    {
-      sequelize,
-      modelName: "User",
-      defaultScope: {
-        attributes: {
-          exclude: ["hashedPassword", "email", "createdAt", "updatedAt"]
-        }
-      },
-      scopes: {
-        currentUser: {
-          attributes: { exclude: ["hashedPassword", "createdAt", "updatedAt"] }
-        },
-        loginUser: {
-          attributes: {}
-        }
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 30],
+      }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 30],
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [3, 256],
+        isEmail: true
+      }
+    },
+    hashedPassword: {
+      type: DataTypes.STRING.BINARY,
+      allowNull: false,
+      validate: {
+        len: [60, 60] // Ensures hashed password is the correct length
       }
     }
-  );
+  }, {
+    sequelize,
+    modelName: 'User',
+    defaultScope: {
+      attributes: {
+        exclude: ['hashedPassword', 'email', 'createdAt', 'updatedAt']
+      }
+    },
+    scopes: {
+      currentUser: {
+        attributes: { exclude: ['hashedPassword', 'createdAt', 'updatedAt'] }
+      },
+      loginUser: {
+        attributes: {}
+      }
+    }
+  });
+
   return User;
 };
+
