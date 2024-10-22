dfb3bd55a69282f7c3595f8b1aa9bf3bce52a45f
Kayleen Sandoval <kayleen.sandoval02@gmail.com>
2024-10-21 20:04:30 -0500

    panda panda panda

--- backend/db/migrations/20241014232605-create-user.js
+++ backend/db/migrations/20241014232605-create-user.js
@@ -1,7 +1,13 @@
-"use strict";
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    return queryInterface.createTable("Users", {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -9,20 +15,12 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       username: {
-        type: Sequelize.TEXT,
+        type: Sequelize.STRING(30),
         allowNull: false,
         unique: true
       },
-      firstName: {
-        type: Sequelize.TEXT,
-        allowNull: false
-      },
-      lastName: {
-        type: Sequelize.TEXT,
-        allowNull: false
-      },
       email: {
-        type: Sequelize.TEXT,
+        type: Sequelize.STRING(256),
         allowNull: false,
         unique: true
       },
@@ -40,9 +38,11 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
-  down: async (queryInterface, Sequelize) => {
-    return queryInterface.dropTable("Users");
+
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Users";
+    return queryInterface.dropTable(options);
   }
 };
