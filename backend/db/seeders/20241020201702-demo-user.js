77021ece451ceec26915c7e3fd566b364c017aa7
wyldee-code <Wyldellizs@gmail.com>
2024-10-22 19:46:38 -0400

    5

--- backend/db/seeders/20241020201702-demo-user.js
+++ backend/db/seeders/20241020201702-demo-user.js
@@ -5,32 +5,39 @@ const bcrypt = require("bcryptjs");
 
 let options = {};
 if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;  // define your schema in options object
+  options.schema = process.env.SCHEMA;  // Define your schema in options object
 }
 
 module.exports = {
   async up (queryInterface, Sequelize) {
+    options.tableName = 'Users';
     await User.bulkCreate([
       {
         email: 'demo@user.io',
         username: 'Demo-lition',
+        firstName: 'Demo',           // Add firstName
+        lastName: 'Lition',          // Add lastName
         hashedPassword: bcrypt.hashSync('password')
       },
       {
         email: 'user1@user.io',
         username: 'FakeUser1',
+        firstName: 'Fake',           // Add firstName
+        lastName: 'User1',           // Add lastName
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
         email: 'user2@user.io',
         username: 'FakeUser2',
+        firstName: 'Fake',           // Add firstName
+        lastName: 'User2',           // Add lastName
         hashedPassword: bcrypt.hashSync('password3')
       }
     ], { validate: true });
   },
 
   async down (queryInterface, Sequelize) {
-    options.tableName = 'Users';
+    options.tableName = 'Users';  
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
       username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
