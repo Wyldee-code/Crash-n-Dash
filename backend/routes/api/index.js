31c1c77e70ddfcf7a8bbafdf60cdb6a1b912392b
wyldee-code <Wyldellizs@gmail.com>
2024-10-23 18:49:47 -0400

    trying something

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const { restoreUser } = require("../../utils/auth.js");
-const spotsRouter = require('./spots');
 
 // Connect restoreUser middleware to the API router
   // If current user session is valid, set req.user to the user in the database
@@ -13,7 +12,6 @@ router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 
-router.use('/spots', spotsRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
