024176ba92948b38e89091052f74b79a58713c59
wyldee-code <Wyldellizs@gmail.com>
2024-10-27 15:48:53 -0400

    1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,19 +1,12 @@
+// routes/index.js
 const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
 
-// router.get('/hello/world', function(req, res) {
-//   res.cookie('XSRF-TOKEN', req.csrfToken());
-//   res.send('Hello World!');
-// });
-
-
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
   res.cookie("XSRF-TOKEN", csrfToken);
-  res.status(200).json({
-    'XSRF-Token': csrfToken
-  });
+  res.status(200).json({ 'XSRF-Token': csrfToken });
 });
 
 router.use('/api', apiRouter);
